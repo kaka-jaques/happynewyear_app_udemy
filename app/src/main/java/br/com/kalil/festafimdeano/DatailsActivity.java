c273d61668ea8e7d8e00b018c675a349a0ccfa3e
kaka-jaques <kjfakhouri@gmail.com>
2022-10-30 12:01:39 -0300

    INITIAL COMMIT!

---
+++ app/src/main/java/br/com/kalil/festafimdeano/DatailsActivity.java
@@ -0,0 +1,14 @@
+package br.com.kalil.festafimdeano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datails);
+    }
+}
